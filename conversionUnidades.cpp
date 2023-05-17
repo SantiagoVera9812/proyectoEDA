3525deda32c95642274c7456bdc2de5cae59063c
Christian Santiago Vera Rojas <104735286+SantiagoVera9812@users.noreply.github.com>
2023-05-17 18:59:45 -0500

    Update conversionUnidades.cpp

--- conversionUnidades.cpp
+++ conversionUnidades.cpp
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
+
 //
 // Created by santiagovera on 24/03/23.
 //
 #include <string>
 #include <math.h>
 #include "funcionalidades.h"
-=======
+
 #include <string>
 #include <math.h>
 #include "funcionalidades.h"
 #include <stdexcept>
->>>>>>> 0711070 (entrega2)
+
 #define MAX_LENGHT 100
 #define UNIDAD_MEDIDA "mt"
 #define UNIDAD_MEDIDA_AREA "mt^2"
@@ -21,7 +21,7 @@
 
 
 float conversionRadianes(std::string unidad_med,std::string valorAConvertir){
-<<<<<<< HEAD
+
 
     float nValor = std::stof(valorAConvertir.c_str());
 
@@ -41,7 +41,7 @@ float conversionRadianes(std::string unidad_med,std::string valorAConvertir){
         help();
     }
 
-=======
+
    
    try{float nValor = std::stof(valorAConvertir.c_str());
 
@@ -66,7 +66,7 @@ float conversionRadianes(std::string unidad_med,std::string valorAConvertir){
     return -1;
       
 }
->>>>>>> 0711070 (entrega2)
+    
 //-----------------------------------------------------------------------------------------------//
 //CONVERSION A METROS, SE PODRA CM,MM Y KM
 //-----------------------------------------------------------------------------------------------//
@@ -74,11 +74,10 @@ float conversionRadianes(std::string unidad_med,std::string valorAConvertir){
 
 float conversionUnidades(std::string unidad_med,std::string valorAConvertir){
 
-<<<<<<< HEAD
     float nValor = std::stof(valorAConvertir.c_str());
-=======
+
    try{ float nValor = std::stof(valorAConvertir.c_str());
->>>>>>> 0711070 (entrega2)
+
     bool comandoValido = true;
     //CONVERTIR DATOS STRING AL TIPO DE DATO QUE SE NECESITA
     for (auto& c: unidad_med){
@@ -91,76 +90,72 @@ float conversionUnidades(std::string unidad_med,std::string valorAConvertir){
         return nValor*factorConversionCM;
     }else if(unidad_med == "MM"){
         return nValor*factorConversionMM;
-<<<<<<< HEAD
+
     }else if(unidad_med == "KM") {
         return nValor*factorConversionKM;
     }else{
         comandoValido = false;
     };
-=======
+
    }else if(unidad_med == "KM") {
         return nValor*factorConversionKM;
     }else{
     comandoValido = false;
 };
->>>>>>> 0711070 (entrega2)
+
 
     if(comandoValido == false){
         printf("por favor vuelva a escribir el comando\n");
         help();
     }
-<<<<<<< HEAD
 
-=======
 }
   catch (const std::invalid_argument& e) {
 
       printf("Comando Invalido");
       return -1;
 }
->>>>>>> 0711070 (entrega2)
+
 }
 
 //-----------------------------------------------------------------------------------------------//
 //CONVERSION A METROS CUADRADOS, SE SOBREENTIENDE mt^2 ASI QUE EL USUARIO DIGITARA CM,MM,KM
 //-----------------------------------------------------------------------------------------------//
 float conversionUnidadesCuadradas(std::string unidad_med,std::string valorAConvertir){
-<<<<<<< HEAD
+
 
     float nValor = std::stof(valorAConvertir.c_str());
 
-=======
+
 try{
     float nValor = std::stof(valorAConvertir.c_str());
     
->>>>>>> 0711070 (entrega2)
     bool comandoValido = true;
     //CONVERTIR DATOS STRING AL TIPO DE DATO QUE SE NECESITA
     for (auto& c: unidad_med){
         c= std::toupper(c);
     } //CONVERTIR A MAYUSCULAS PARA HACER VALIDACION
 
-<<<<<<< HEAD
     float res;
     if(unidad_med == "MT"){
         return nValor;
     }else if(unidad_med == "CM"){
         float cm = factorConversionCM;
         res=pow(cm,2);
-=======
+
   float res;
     if(unidad_med == "MT"){
         return nValor;
     }else if(unidad_med == "CM"){
       float cm = factorConversionCM;
       res=pow(cm,2);
->>>>>>> 0711070 (entrega2)
+
         return nValor*res;
     }else if(unidad_med == "MM"){
         float mm = factorConversionMM;
         res = pow(mm,2);
         return nValor*res;
-<<<<<<< HEAD
+
     }else if(unidad_med == "KM") {
         float km = factorConversionKM;
         res = pow(km,2);
@@ -168,7 +163,7 @@ try{
     }else{
         comandoValido = false;
     };
-=======
+
    }else if(unidad_med == "KM") {
       float km = factorConversionKM;
       res = pow(km,2);
@@ -176,20 +171,19 @@ try{
     }else{
     comandoValido = false;
 };
->>>>>>> 0711070 (entrega2)
+
 
     if(comandoValido == false){
         printf("Por favor vuelva a escribir el comando\n");
         help();
     }
-<<<<<<< HEAD
-=======
+
   }
   catch (const std::invalid_argument& e) {
 
       printf("Comando Invalido");
       return -1;
 }
->>>>>>> 0711070 (entrega2)
+
 
 }
