0f80ece1cee4e6c578ea68781333ed47d870db22
Christian Santiago Vera Rojas <104735286+SantiagoVera9812@users.noreply.github.com>
2023-05-17 19:00:50 -0500

    Update conversionUnidades.h

--- conversionUnidades.h
+++ conversionUnidades.h
@@ -1,16 +1,14 @@
-<<<<<<< HEAD
+
 //
 // Created by santiagovera on 24/03/23.
 //
 
-=======
->>>>>>> 0711070 (entrega2)
 #pragma once
 #include <string>
 float conversionUnidadesCuadradas(std::string unidad_med,std::string valorAConvertir);
 float conversionUnidades(std::string unidad_med,std::string valorAConvertir);
-<<<<<<< HEAD
+
 float conversionRadianes(std::string unidad_med,std::string valorAConvertir);
-=======
+
 float conversionRadianes(std::string unidad_med,std::string valorAConvertir);
->>>>>>> 0711070 (entrega2)
+
